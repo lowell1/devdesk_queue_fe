d8411116763a4c3011c5c2018a9b783d9482575d
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-10-24 00:03:06 -0600

    imported bootstrap css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {createStore, applyMiddleware} from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 import {Provider} from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
